8625e920a7d1928ddfb772056cab99c279b997b6
Lewis Gaul <>
2021-05-07 00:06:09 +0100

    Update README and bump to v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="py-nestedtext",
-    version="0.0.1",
+    version="0.0.2",
     description="Minimal implementation of NestedText data format",
     author="Lewis Gaul",
     author_email="lewis.gaul@gmail.com",
